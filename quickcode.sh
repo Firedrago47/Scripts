c75d6bf97b41e1dda6593325092f72684e368ae7
Deep <deepanrajpmk@gmail.com>
2025-12-06 00:03:15 +0530

    feat:script update

--- quickcode.sh
+++ quickcode.sh
@@ -6,9 +6,19 @@ read pname
 
 echo "Opening the project..."
 
-cd Documents/Projects/$pname
+cd ~/Documents/Projects/$pname
 
-code .
+echo "Which code editor?"
+
+read editor
+
+if [[ $editor == "vscode" ]]; then
+	code .
+elif [[ $editor == "nvim" ]]; then
+	nvim $pname
+else
+	echo "sorry!only vscode and nvim will work!"
+fi
 
 sleep 2
 
